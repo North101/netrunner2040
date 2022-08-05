109c88fda099be95c1e7351772868cd07718439d
Alexander Wilde <alexander.wilde87@gmail.com>
2022-08-05 14:53:53 +0100

    use shared display

--- netrunner2040/widgets/app.py
+++ netrunner2040/widgets/app.py
@@ -5,7 +5,7 @@ from .tracker_screen import TrackerScreen
 
 
 class MyApp(App):
-  def __init__(self, display: badger2040.Badger2040):
-    super().__init__(display=display)
+  def __init__(self):
+    super().__init__()
 
     self.child = TrackerScreen()
